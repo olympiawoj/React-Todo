f07372e2b145e68b881500a4d9fdbbd1a5ce40cb
olympiawojcik <owojcik7@gmail.com>
2019-02-14 17:17:03 -0500

    create TodoList Component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React from "react";
+import TodoList from "./components/TodoComponents/TodoList";
 
-//Step 1: Declare class component and extend the React.Component base class
+const todosArray = [
+  {
+    task: "Organize Garage",
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: "Bake Cookies",
+    id: 1528817084358,
+    completed: false
+  },
+  {
+    task: "DO THE THING",
+    id: 15288170,
+    completed: false
+  },
+  {
+    task: "Do the other thing",
+    id: 152858,
+    completed: false
+  }
+];
 
 class App extends React.Component {
-  // You will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-
-  //Step 2: Set-up our constructor and add state. State is an object that holds our data. It allows us to drive UI using data. Super(); which we declare before state, gives us access to our parents class methods. Inside this.state{} is where we add properties to state data.
   constructor() {
     super();
     this.state = {
+      todosArray,
       test: "This is a test"
     };
   }
-
-  //Step 3: Render is a function which returns our JSX. Through interpolation, we can reference the state object by using the *THIS* keyword which points to an object on the class constructor
 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
         <h2>{this.state.test}</h2>
+        <TodoList todosArray={this.state.todosArray} />
       </div>
     );
   }
